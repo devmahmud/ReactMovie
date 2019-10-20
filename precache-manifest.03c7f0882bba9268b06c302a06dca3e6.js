8499bf6741c7df9c435880ff87084b3124ea9ca4
devmahmud <expelmahmud@gmail.com>
2019-10-20 11:28:48 +0600

    Updates

---
+++ precache-manifest.03c7f0882bba9268b06c302a06dca3e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be5ebea1802a9e00914268539812a48",
+    "url": "/ReactMovie/index.html"
+  },
+  {
+    "revision": "b4e4f17bb2f21ba8ff34",
+    "url": "/ReactMovie/static/css/main.7637e283.chunk.css"
+  },
+  {
+    "revision": "3785b47f615cc43bd746",
+    "url": "/ReactMovie/static/js/2.536c854a.chunk.js"
+  },
+  {
+    "revision": "b4e4f17bb2f21ba8ff34",
+    "url": "/ReactMovie/static/js/main.3d8ecacc.chunk.js"
+  },
+  {
+    "revision": "326b3b2a2b70cd3edc19",
+    "url": "/ReactMovie/static/js/runtime-main.428b5b15.js"
+  }
+]);

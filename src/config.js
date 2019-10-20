761db4b9222afdd59bc8bdebc90a08f40bd83729
devmahmud <expelmahmud@gmail.com>
2019-10-20 20:50:31 +0600

    Secure connection issue for static images fixed

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
 // An image URL looks like this example:
 // http://image.tmdb.org/t/p/w780/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg
 
-const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
 
 //Sizes: w300, w780, w1280, original
 const BACKDROP_SIZE = "w1280";
